b9b108ca480eb3351056fce62d2565b3ee766404
student <i.i.f@mail.ru>
2022-08-01 10:00:44 +0800

    Аватары

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -1,10 +1,9 @@
 package ru.hogwarts.school.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
 
-import java.util.Optional;
-
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-        Optional<Avatar> findByStudentId(Long studentId);
+    Optional<Avatar> findByStudentId(Long studentId);
 }
